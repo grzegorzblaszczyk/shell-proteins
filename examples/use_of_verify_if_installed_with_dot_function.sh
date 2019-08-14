15b8557234416e7a75ce9604f871f1788874a574
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2019-08-15 00:45:58 +0200

    ADD: example for detection of operating system and then show proper install message

---
+++ examples/use_of_verify_if_installed_with_dot_function.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source "$HOME/bin/shell-proteins/bash/base.inc.sh"
+
+protein_require "grzegorzblaszczyk/shell-proteins-ext/bash/os/get_os_version" "https://raw.githubusercontent.com/grzegorzblaszczyk/shell-proteins-ext/master/bash/os/get_os_version.inc.sh"
+os_version="`get_os_version`"
+echo "os version: ${os_version}"
+
+case "$os_version" in
+MacOSX)
+  os="mac"
+  ;;
+Ubuntu)
+  os="ubuntu"
+  ;;
+*)
+  os="unknow"
+  ;;
+esac
+
+protein_require "grzegorzblaszczyk/shell-proteins-ext/bash/${os}/verify_if_installed_with_dot" "https://raw.githubusercontent.com/grzegorzblaszczyk/shell-proteins-ext/master/bash/mac/verify_if_installed_with_dot.inc.sh"
+
+verify_if_installed_with_dot "convert" "/usr/local/bin/convert"
+
+echo ""
+echo "Assuming that foo-nonexistent is NOT installed..."
+
+verify_if_installed_with_dot "foo-nonexistent" "/usr/bin/foo-nonexistent"
+
+
